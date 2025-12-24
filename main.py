3c0989c3661b02efe1bf102e20d6e89e208d0862
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2025-12-24 22:55:31 +0530

    Python to do Basic Coding

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-input_prompt = "Enter to Do: "
-user_text = input(input_prompt)
-print(user_text)
-print(type(user_text))
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.lower().strip()
+    match user_action:
+        case "add" | "input":
+            todo = input("Enter a todo: ")
+            todos.append(todo.strip())
+        case "show" | "display":
+            for item in todos:
+                print(item)
+        case "edit" | "change" | "update":
+            index = int(input("Enter the index of the todo to edit: "))
+            index -= 1
+            new_todo = input("Enter new todo:")
+            todos[index] = new_todo
+        case "exit" | "quit":
+            break
+        case _:
+            print("Invalid input")
+            continue
+
+print ("Goodbye")
