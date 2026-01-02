b8ede551d4d0aa6f44933d2fa5b4393d6ab87a71
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-01-02 22:56:52 +0530

    Changed match case to if else

--- main.py
+++ main.py
@@ -1,68 +1,66 @@
 todos = []
 while True:
-    user_action = input("Type assign, show, edit, complete or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.lower().strip()
 
-    match user_action:
+    if "add" in user_action:
 
-        case "add" | "input"| "assign":
+        todo = user_action[4:]
 
-            todo = input("Enter a todo: ") + "\n"
+        with open(f"todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open(f"todos.txt", "r") as file:
-                todos = file.readlines()
+        todos.append(todo)
 
-            todos.append(todo)
-
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
 
-        case "show" | "display":
+    elif "show" in user_action:
 
-            with open(f"todos.txt", "r") as file:
-                todos = file.readlines()
+        with open(f"todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index,item in enumerate(todos):
-                print(f"{index+1}-{item.strip("\n")}")
-
-
-        case "edit" | "change" | "update":
-
-            index = int(input("Enter the index of the todo to edit: "))
-            index -= 1
-
-            with open(f"todos.txt", "r") as file:
-                todos = file.readlines()
-
-            todo_to_change = todos[index]
-
-            new_todo = input("Enter new todo:")
-            todos[index] = new_todo + "\n"
-
-            with open(f"todos.txt", "w") as file:
-                file.writelines(todos)
-
-            print(f"Todo {todo_to_change.strip("\n")} has been changed to {new_todo}")
+        for index,item in enumerate(todos):
+            print(f"{index+1}-{item.strip("\n")}")
 
 
-        case "complete"| "finish":
-            with open(f"todos.txt", "r") as file:
-                todos = file.readlines()
+    elif "edit" in user_action:
 
-            index = int(input("Position of the todo to complete: ")) -1
-            todos.pop(index)
+        index = int(user_action[5:])
+        index -= 1
 
-            with open(f"todos.txt", "w") as file:
-                file.writelines(todos)
+        with open(f"todos.txt", "r") as file:
+            todos = file.readlines()
+
+        todo_to_change = todos[index]
+
+        new_todo = input("Enter new todo:")
+        todos[index] = new_todo + "\n"
+
+        with open(f"todos.txt", "w") as file:
+            file.writelines(todos)
+
+        print(f"Todo {todo_to_change.strip("\n")} has been changed to {new_todo}")
 
 
-        case "exit" | "quit"| "bye":
-            break
+    elif "complete" in user_action:
+        with open(f"todos.txt", "r") as file:
+            todos = file.readlines()
+
+        index = int(user_action[9:]) -1
+        todos.pop(index)
+
+        with open(f"todos.txt", "w") as file:
+            file.writelines(todos)
 
 
-        case _:
-            print("Invalid input")
-            continue
+    elif "exit" in user_action:
+        break
+
+
+    else:
+        print("Invalid input")
+        continue
 
 print ("Goodbye")
