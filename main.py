2f66e84574f6d68f58efa62df6bf9ce559634117
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-01-03 22:12:51 +0530

    Big fixes and handling Value Error and IndexError

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.lower().strip()
 
-    if "add" in user_action:
+    if user_action.startswith("add"):
 
         todo = user_action[4:]
 
         with open(f"todos.txt", "r") as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + "\n")
 
         with open("todos.txt", "w") as file:
             file.writelines(todos)
 
 
-    elif "show" in user_action:
+    elif user_action.startswith("show"):
 
         with open(f"todos.txt", "r") as file:
             todos = file.readlines()
@@ -25,37 +25,52 @@ while True:
             print(f"{index+1}-{item.strip("\n")}")
 
 
-    elif "edit" in user_action:
+    elif user_action.startswith("edit"):
 
-        index = int(user_action[5:])
-        index -= 1
+        try:
+            index = int(user_action[5:])
+            index -= 1
 
-        with open(f"todos.txt", "r") as file:
-            todos = file.readlines()
+            with open(f"todos.txt", "r") as file:
+                todos = file.readlines()
 
-        todo_to_change = todos[index]
+            todo_to_change = todos[index]
 
-        new_todo = input("Enter new todo:")
-        todos[index] = new_todo + "\n"
+            new_todo = input("Enter new todo:")
+            todos[index] = new_todo + "\n"
 
-        with open(f"todos.txt", "w") as file:
-            file.writelines(todos)
+            with open(f"todos.txt", "w") as file:
+                file.writelines(todos)
 
-        print(f"Todo {todo_to_change.strip("\n")} has been changed to {new_todo}")
+            print(f"Todo {todo_to_change.strip("\n")} has been changed to {new_todo}")
+
+        except ValueError:
+            print("Invalid command! Syntax for Edit Command is edit <index of list to be changed>")
+            continue
+        except IndexError:
+            print("Invalid Index Number! Please enter a valid index")
+            continue
+
+    elif user_action.startswith("complete"):
+
+        try:
+            with open(f"todos.txt", "r") as file:
+                todos = file.readlines()
+
+            index = int(user_action[9:]) -1
+            todos.pop(index)
+
+            with open(f"todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Invalid command! Syntax for Edit Command is edit <index of list to be changed>")
+            continue
+        except IndexError:
+            print("Invalid Index Number! Please enter a valid index")
+            continue
 
 
-    elif "complete" in user_action:
-        with open(f"todos.txt", "r") as file:
-            todos = file.readlines()
-
-        index = int(user_action[9:]) -1
-        todos.pop(index)
-
-        with open(f"todos.txt", "w") as file:
-            file.writelines(todos)
-
-
-    elif "exit" in user_action:
+    elif user_action.startswith("exit"):
         break
 
 
