07d538eb565a8f1b1c8216bbbb4e5b5eb1e6c39f
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-01-04 22:43:55 +0530

    Replaced repetative code with functions

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 todos = []
+
+def get_todos():
+    with open(f"todos.txt", "r") as file:
+        todos = file.readlines()
+    return todos
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.lower().strip()
+    user_action = user_action.strip()
 
     if user_action.startswith("add"):
 
         todo = user_action[4:]
 
-        with open(f"todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
@@ -18,8 +23,7 @@ while True:
 
     elif user_action.startswith("show"):
 
-        with open(f"todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         for index,item in enumerate(todos):
             print(f"{index+1}-{item.strip("\n")}")
@@ -31,8 +35,7 @@ while True:
             index = int(user_action[5:])
             index -= 1
 
-            with open(f"todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             todo_to_change = todos[index]
 
@@ -54,8 +57,7 @@ while True:
     elif user_action.startswith("complete"):
 
         try:
-            with open(f"todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             index = int(user_action[9:]) -1
             todos.pop(index)
