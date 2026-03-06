d92869dbdf583d98b0520b58ddfb23148a79382d
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-03-06 23:35:00 +0530

    Implemented Add functionality in To Do GUI

--- gui.py
+++ gui.py
@@ -1,13 +1,26 @@
 #Importing Functions
 import functions
-import FreeSimpleGUI as sg
+import FreeSimpleGUI as gui
 
 #UI Window for To Do App
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a to-do")
-add_button = sg.Button("Add")
+label = gui.Text("Type in a to-do")
+input_box = gui.InputText(tooltip="Enter a to-do",key="todo")
+add_button = gui.Button("Add")
 
-window = sg.Window("my to Do App", layout=[[label],[input_box,add_button]])
-window.read()
+window = gui.Window("my to Do App",
+                   layout=[[label],
+                           [input_box,add_button]],
+                   font=('Helvetica', 20))
+while True:
+    event, values =window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case gui.WIN_CLOSED:
+            break
+
 window.close()
 
