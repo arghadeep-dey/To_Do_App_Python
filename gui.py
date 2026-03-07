1fbdb47af11b7c77a8926828a2173c715b274173
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-03-07 18:37:34 +0530

    Implemented Edit functionality in To Do GUI

--- gui.py
+++ gui.py
@@ -6,10 +6,14 @@ import FreeSimpleGUI as gui
 label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter a to-do",key="todo")
 add_button = gui.Button("Add")
+edit_button = gui.Button("Edit")
+list_box = gui.Listbox(values = functions.get_todos(), key = 'todos',
+                       enable_events=True, size=[45,10])
 
 window = gui.Window("my to Do App",
                    layout=[[label],
-                           [input_box,add_button]],
+                           [input_box,add_button],
+                           [list_box,edit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values =window.read()
@@ -19,6 +23,19 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            inex = todos.index(todo_to_edit)
+            todos[inex] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+        case 'todos':
+            window['todos'].update(values = values['todos'][0])
+
         case gui.WIN_CLOSED:
             break
 
