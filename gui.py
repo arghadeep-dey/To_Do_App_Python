461fdfca4dfc4ff2572dc2f2b41b7611d2d4b44c
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-03-08 23:20:24 +0530

    Bug Fix - using tuple in ListBox

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ input_box = gui.InputText(tooltip="Enter a to-do",key="todo")
 add_button = gui.Button("Add")
 edit_button = gui.Button("Edit")
 list_box = gui.Listbox(values = functions.get_todos(), key = 'todos',
-                       enable_events=True, size=[45,10])
+                       enable_events=True, size=(45,10))
 
 window = gui.Window("my to Do App",
                    layout=[[label],
@@ -24,15 +24,17 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+
         case "Edit":
             todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            new_todo = values['todo']+ "\n"
 
             todos = functions.get_todos()
             inex = todos.index(todo_to_edit)
             todos[inex] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values = todos)
+
         case 'todos':
             window['todos'].update(values = values['todos'][0])
 
